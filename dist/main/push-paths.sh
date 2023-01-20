10bf5f746430948bf6c46c145b5fe3d317ead8a2
Ulrik Strid <ulrik.strid@outlook.com>
2023-01-20 10:31:28 +0100

    Use parallel for loop for upload

--- dist/main/push-paths.sh
+++ dist/main/push-paths.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 
 nixArgs=${1:--j8} endpoint=$2 pathsToPush=$3 pushFilter=$4
 
+if [[ $endpoint == "" ]]; then
+    echo "No endpoint set, exiting"
+    exit 1
+fi
+
 if [[ $pathsToPush == "" ]]; then
     pathsToPush=$(comm -13 <(sort /tmp/store-path-pre-build) <("$(dirname "$0")"/list-nix-store.sh))
 
@@ -11,4 +16,6 @@ if [[ $pathsToPush == "" ]]; then
     fi
 fi
 
-echo "$pathsToPush" | nix copy --verbose $nixArgs --to "$endpoint"
+for p in $pathsToPush; do
+    nix copy --verbose $nixArgs --to "$endpoint" "$p" &
+done
