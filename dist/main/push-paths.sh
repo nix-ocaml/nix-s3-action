9a17ac369946e4dc1279d3409e0dc27f2d28d566
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-01-25 08:40:59 -0800

    Revert "Use parallel for loop for upload"
    This reverts commit 10bf5f746430948bf6c46c145b5fe3d317ead8a2.

--- dist/main/push-paths.sh
+++ dist/main/push-paths.sh
@@ -3,11 +3,6 @@ set -xeuo pipefail
 
 nixArgs=${1:--j8} endpoint=$2 pathsToPush=$3 pushFilter=$4
 
-if [[ $endpoint == "" ]]; then
-    echo "No endpoint set, exiting"
-    exit 1
-fi
-
 if [[ $pathsToPush == "" ]]; then
     pathsToPush=$(comm -13 <(sort /tmp/store-path-pre-build) <("$(dirname "$0")"/list-nix-store.sh))
 
